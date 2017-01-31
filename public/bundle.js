2aee69b3727a7b6fc454a010b66cc35f4927fbd1
Matt Walters <mattwalters5@gmail.com>
2017-01-31 15:15:32 -0500

    updating TOS

--- public/bundle.js
+++ public/bundle.js
@@ -63,7 +63,7 @@
 /******/ 	}
 
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "fa11d5107a71e72d1e30"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "568af97404d64386deeb"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 
@@ -24172,46 +24172,120 @@
 	          "div",
 	          { className: "container" },
 	          _react2.default.createElement(
-	            "svg",
-	            { width: "68px", height: "68px", viewBox: "0 0 68 68", version: "1.1" },
+	            "div",
+	            { className: "title" },
 	            _react2.default.createElement(
-	              "g",
-	              { id: "Designs-r2", stroke: "none", strokeWidth: "1", fill: "none", fillRule: "evenodd" },
+	              "svg",
+	              { width: "68px", height: "68px", viewBox: "0 0 68 68", version: "1.1" },
 	              _react2.default.createElement(
 	                "g",
-	                { id: "A", transform: "translate(-62.000000, -69.000000)", fill: "#000000" },
+	                { id: "Designs-r2", stroke: "none", strokeWidth: "1", fill: "none", fillRule: "evenodd" },
 	                _react2.default.createElement(
 	                  "g",
-	                  { id: "dotBC-logo", transform: "translate(62.000000, 69.000000)" },
-	                  _react2.default.createElement("path", { d: "M67.1296039,41.6774194 C67.6990576,39.2101891 68,36.640239 68,34 C68,15.2223185 52.7776815,0 34,0 C25.915686,0 18.4903734,2.82152055 12.6550868,7.53353703 L12.6550868,16.6785812 C17.6512505,10.5180859 25.2814387,6.58064516 33.8312655,6.58064516 C48.8813675,6.58064516 61.0818859,18.7811635 61.0818859,33.8312655 C61.0818859,36.5587962 60.681168,39.1927343 59.9353926,41.6774194 L67.1296039,41.6774194 Z M64.7963648,48.426799 C59.3693703,59.9916004 47.6199161,68 34,68 C15.2223185,68 0,52.7776815 0,34 C0,26.4764204 2.44368884,19.523595 6.58064516,13.891945 L6.58064516,33.9156328 C6.62626674,48.9269365 18.8092959,61.0818859 33.8312655,61.0818859 C43.5119323,61.0818859 52.0136011,56.0339952 56.8476868,48.426799 L64.7963648,48.426799 Z", id: "Combined-Shape" }),
-	                  _react2.default.createElement("path", { d: "M34.0421836,41.5930521 C38.1192236,41.5930521 41.4243176,38.2879581 41.4243176,34.2109181 C41.4243176,30.1338781 38.1192236,26.8287841 34.0421836,26.8287841 C29.9651436,26.8287841 26.6600496,30.1338781 26.6600496,34.2109181 C26.6600496,38.2879581 29.9651436,41.5930521 34.0421836,41.5930521 Z", id: "Oval-6" }),
-	                  _react2.default.createElement("path", { d: "M48.426799,18.2090421 C52.7791282,22.1457852 55.5136476,27.8379192 55.5136476,34.1687345 C55.5136476,46.050394 45.8816595,55.6823821 34,55.6823821 C22.1183405,55.6823821 12.4863524,46.050394 12.4863524,34.1687345 C12.4863524,22.287075 22.1183405,12.6550868 34,12.6550868 C36.4515495,12.6550868 38.8073253,13.0651427 41.0024814,13.8204085 L41.0024814,21.2496974 C38.9066478,20.0745186 36.4893898,19.4044665 33.9156328,19.4044665 C25.9013369,19.4044665 19.4044665,25.9013369 19.4044665,33.9156328 C19.4044665,41.9299286 25.9013369,48.426799 33.9156328,48.426799 C41.8736082,48.426799 48.3354107,42.020921 48.4258382,34.0843672 L48.426799,34.0843672 L48.426799,18.2090421 Z", id: "Combined-Shape" })
+	                  { id: "A", transform: "translate(-62.000000, -69.000000)", fill: "#000000" },
+	                  _react2.default.createElement(
+	                    "g",
+	                    { id: "dotBC-logo", transform: "translate(62.000000, 69.000000)" },
+	                    _react2.default.createElement("path", { d: "M67.1296039,41.6774194 C67.6990576,39.2101891 68,36.640239 68,34 C68,15.2223185 52.7776815,0 34,0 C25.915686,0 18.4903734,2.82152055 12.6550868,7.53353703 L12.6550868,16.6785812 C17.6512505,10.5180859 25.2814387,6.58064516 33.8312655,6.58064516 C48.8813675,6.58064516 61.0818859,18.7811635 61.0818859,33.8312655 C61.0818859,36.5587962 60.681168,39.1927343 59.9353926,41.6774194 L67.1296039,41.6774194 Z M64.7963648,48.426799 C59.3693703,59.9916004 47.6199161,68 34,68 C15.2223185,68 0,52.7776815 0,34 C0,26.4764204 2.44368884,19.523595 6.58064516,13.891945 L6.58064516,33.9156328 C6.62626674,48.9269365 18.8092959,61.0818859 33.8312655,61.0818859 C43.5119323,61.0818859 52.0136011,56.0339952 56.8476868,48.426799 L64.7963648,48.426799 Z", id: "Combined-Shape" }),
+	                    _react2.default.createElement("path", { d: "M34.0421836,41.5930521 C38.1192236,41.5930521 41.4243176,38.2879581 41.4243176,34.2109181 C41.4243176,30.1338781 38.1192236,26.8287841 34.0421836,26.8287841 C29.9651436,26.8287841 26.6600496,30.1338781 26.6600496,34.2109181 C26.6600496,38.2879581 29.9651436,41.5930521 34.0421836,41.5930521 Z", id: "Oval-6" }),
+	                    _react2.default.createElement("path", { d: "M48.426799,18.2090421 C52.7791282,22.1457852 55.5136476,27.8379192 55.5136476,34.1687345 C55.5136476,46.050394 45.8816595,55.6823821 34,55.6823821 C22.1183405,55.6823821 12.4863524,46.050394 12.4863524,34.1687345 C12.4863524,22.287075 22.1183405,12.6550868 34,12.6550868 C36.4515495,12.6550868 38.8073253,13.0651427 41.0024814,13.8204085 L41.0024814,21.2496974 C38.9066478,20.0745186 36.4893898,19.4044665 33.9156328,19.4044665 C25.9013369,19.4044665 19.4044665,25.9013369 19.4044665,33.9156328 C19.4044665,41.9299286 25.9013369,48.426799 33.9156328,48.426799 C41.8736082,48.426799 48.3354107,42.020921 48.4258382,34.0843672 L48.426799,34.0843672 L48.426799,18.2090421 Z", id: "Combined-Shape" })
+	                  )
 	                )
 	              )
+	            ),
+	            _react2.default.createElement(
+	              "span",
+	              { className: "name" },
+	              "dotBC Music"
+	            ),
+	            _react2.default.createElement(
+	              "a",
+	              { href: "http://dotblockchainmusic.com" },
+	              "dotblockchainmusic.com"
+	            ),
+	            _react2.default.createElement(
+	              "span",
+	              null,
+	              "\xA9 2016"
 	            )
-	          ),
-	          _react2.default.createElement(
-	            "span",
-	            { className: "name" },
-	            "dotBC Music"
-	          ),
-	          _react2.default.createElement(
-	            "a",
-	            { href: "http://dotblockchainmusic.com" },
-	            "dotblockchainmusic.com"
-	          ),
-	          _react2.default.createElement(
-	            "span",
-	            null,
-	            "\xA9 2016"
 	          ),
 	          _react2.default.createElement(
 	            "div",
 	            { className: "legal" },
 	            _react2.default.createElement(
+	              "h3",
+	              { className: "conditions" },
+	              "DOTBC UPLOADER TERMS AND CONDITIONS"
+	            ),
+	            _react2.default.createElement(
 	              "p",
 	              null,
-	              " Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	              "These terms and conditions (this \u201CAgreement\u201D) by and between Dot Blockchain Music, Inc. (\u201CDotBC\u201D or \u201CWe\u201D) and you (\u201CYou\u201D) govern provision of the DotBC beta testing uploader service (the \u201CService\u201D), and is effective as of the moment You (a) utilize the Service or (b) establish an account with DotBC using DotBC\u2019s registration service. DotBC may revise this Agreement from time to time by posting a new version hereof on the DotBC Website. Your continued use of the Service constitutes acceptance of such revisions to this Agreement."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "1. The Service includes the DotBC uploader tool which allows You to upload rights ownership data and information into the DotBC system for referencing and logging into a blockchain test environment."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "2. This Agreement will continue until terminated by either party. Your continued use of the Service indicates Your continued acceptance of this Agreement. DotBC may restrict or discontinue your access to the Service at any time for any reason."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "3. You warrant that You will not, using the Service to: (i) perpetrate any security breach, network attack, act of hacking, or distribution of any virus, worm, or other harmful code; (ii) perpetrate any fraud or intellectual property infringement; (iii) disseminate or post any material or information that is or may be threatening, libelous, obscene, harassing, offensive or knowingly false; (iv) threaten or harass any DotBC employee, agent, or representative by telephone, in person, or through any other means of communication; (v) perpetrate any crime or other illegal activity; (vi) tamper with other DotBC accounts, commit unauthorized intrusion into any part of DotBC\u2019s system; (vii) scan or scrape any data in the Service;"
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "4. DotBC has no obligation to monitor the Service but may do so and may disclose information regarding use of the Service for any reason, including: to satisfy laws, regulations, or governmental, legal, or law-enforcement requests; to operate the Service properly; or to protect itself and its customers. DotBC will use all commercially reasonable efforts to protect Your data from dissemination to third parties during the testing phase, and We will provide you at least ten (10) business days prior written notification if the Service will be put into a live environment."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "5. The Service provided by DotBC is licensed to You and remains DotBC\u2019s sole and exclusive property, unless otherwise covered and licensed under an open source license. You retain ownership of all of Your data uploaded through the Service, but You hereby grant to DotBC and its approved third party suppliers, partners, and other customers a revocable, no- charge license to use Your data solely for testing of the Service and DotBC\u2019s rights ownership reconciliation services."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "6. DotBC may interrupt the Service to perform maintenance. You are responsible for maintaining security, for maintaining patches and disaster recovery systems, and for maintaining backups. DotBC will not be liable for loss of data or for breaches in system integrity. You agree to promptly report any Service failures to DotBC."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "7. Disclaimers and Warranties."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "(a) THE SERVICE IS PROVIDED ON AN \u201CAS IS\u201D AND \u201CAS AVAILABLE\u201D BASIS. DOTBC DOES NOT WARRANT THAT THE SERVICE WILL BE UNINTERRUPTED, ERROR-FREE, OR FREE FROM VIRUSES OR OTHER HARMFUL COMPONENTS. DOTBC MAKES NO EXPRESS OR IMPLIED WARRANTIES WHATSOEVER, INCLUDING WITHOUT LIMITATION, WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "(b) DOTBC WILL NOT BE LIABLE FOR ANY CONSEQUENTIAL, INCIDENTAL, EXEMPLARY, PUNITIVE, OR MULTIPLE DAMAGES, EVEN IF DOTBC WAS ADVISED IN ADVANCE OF THE POSSIBILITY OF SUCH DAMAGES. DOTBC\u2019S MAXIMUM LIABILITY ARISING OUT OF OR RELATED TO THIS AGREEMENT OR THE SERVICE WILL NOT EXCEED FIVE HUNDRED DOLLARS ($500)."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "(c) DOTBC WILL HAVE NO LIABILITY WHATSOEVER FOR ANY CLAIMS, LOSSES, ACTIONS, DAMAGES, SUITS, OR PROCEEDINGS RESULTING FROM: (i) THIRD PARTIES ACCESSING YOUR DATA OR ASSIGNED COMPUTERS; (ii) SECURITY BREACHES; (iii) EAVESDROPPING; (iv) DENIAL OF SERVICE ATTACKS; (v) INTERCEPTION OF TRAFFIC SENT OR RECEIVED USING THE EQUIPMENT OR SERVICE; (vi) YOUR RELIANCE ON OR USE OF THE SERVICE; (vii) MISTAKES, OMISSIONS, INTERRUPTIONS, DELETIONS OF FILES, ERRORS, DEFECTS, DELAYS IN OPERATION, OR OTHER FAILURES OF PERFORMANCE OF THE EQUIPMENT OR SERVICE; (viii) THE ACCURACY, COMPLETENESS, AND USEFULNESS OF THE SERVICE; OR (ix) LOSS OF DATA OR LOSS OF ACCESS TO DATA."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "(d) DOTBC\u2019S LIMITATIONS AND EXCLUSIONS OF LIABILITY SET FORTH IN THIS SECTION AND IN THIS AGREEMENT APPLY EQUALLY TO DOTBC\u2019S OFFICERS, EMPLOYEES, AGENTS, CONTRACTORS, REPRESENTATIVES, SUPPLIERS, SUBSIDIARIES, PARENTS, AND AFFILIATED COMPANIES."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "(e) YOU WARRANT THAT YOU HAVE THE RIGHT AND AUTHORITY TO UPLOAD YOUR INFORMATION TO THE SERVICE AND THAT, TO THE BEST OF YOUR KNOWLEDGE, THE DATA YOU UPLOAD AND ENTER INTO THE SERVICE IS CORRECT."
+	            ),
+	            _react2.default.createElement(
+	              "p",
+	              null,
+	              "9. Miscellaneous. This Agreement will be construed in accordance with and governed by the laws of the State of New York without giving effect to any choice of law rules. The parties hereby consent to the personal and exclusive jurisdiction and venue of the federal and state courts of New York, New York. No delay, failure, or waiver of either party\u2019s exercise or partial exercise of any right or remedy under this Agreement will operate to limit, impair, preclude, cancel, waive, or otherwise affect such right or remedy. If any provision of this Agreement is held invalid, illegal, or unenforceable, the validity, legality, or enforceability of the remaining provisions will in no way be affected or impaired thereby. This Agreement, with any other instrument or document referenced herein, which are incorporated by reference as though set forth herein, embodies the final, full, and exclusive statement of the agreement between the parties, and as of its date supersedes all prior agreements, negotiations, representations, and proposals, written or oral, relating to the subject-matter hereof. All written communications to You will be deemed delivered if sent to the contact information provided to Your address given at registration, unless You provide alternate contact information in writing. All written communications to DotBC shall be mailed to Dot Blockchain Music, Inc., 99 Gold Street Suite 1C, Brooklyn, NY 11201."
 	            )
 	          )
 	        )
